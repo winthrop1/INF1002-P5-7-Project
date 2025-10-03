a4425ae259b83407e86e5a9b7fb6017040960715
thewy323 <matthewpdyason@gmail.com>
2025-10-03 14:23:36 +0800

    matt

--- main.py
+++ main.py
@@ -10,10 +10,15 @@ final_score = 0
 file_path = " input of user " 
 
 
+
+
 get_urls_from_email_file(file_path)
 
 calling_all_functions()
 assessing_risk_scores()
+
+
+
 
 #suspicious url score out of 5
 #

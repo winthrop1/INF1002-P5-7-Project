8063ab334a60dde5f510bfa5a0b09fef81936281
thewy323 <matthewpdyason@gmail.com>
2025-10-03 15:57:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import datas
-import detectfunction
-import spamwords
 from suspiciousurl import calling_all_functions, assessing_risk_scores, get_urls_from_email_file
 
 
 final_score = 0
 
 
-file_path = " input of user " 
+file_path = r"C:\Users\User\Documents\GitHub\INF1002-P5-7-Project\spam\spam_1.txt" 
 
 
 
